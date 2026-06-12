37dba809aedcfeeb649c06d31af3fcddeed48148
Leticia Bezerra <leticiabezerrauy@gmail.com>
2026-06-12 00:16:43 -0300

    feat(resource): add UserResource to control API output

---
+++ app/Http/Resources/Api/V1/UserResource.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'            => $this->id,
+            'name'          => $this->name,
+            'email'         => $this->email,
+            'role'          => $this->role->value,
+            'country'       => $this->country,
+            'currency_code' => $this->currency_code,
+        ];
+    }
+}
